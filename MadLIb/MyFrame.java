5eadee10ac5f507e966dd6e41cae91c80a5774a3
joflores18 <joflores@student.neumont.edu>
2022-08-16 10:37:44 -0600

    Panel Changes

--- MadLIb/MyFrame.java
+++ MadLIb/MyFrame.java
@@ -13,23 +13,10 @@ public class MyFrame {
         JFrame f;
         f = new JFrame("MadLib+");
 
-        JButton enterB;
-        enterB = new JButton("Enter");
-        enterB.setBackground(Color.white);
-        enterB.setBounds(150, 200, 75, 25);
-
-        JTextField inputF;
-        inputF = new JTextField();
-        inputF.setBounds(250, 200, 75, 25);
-
         JButton enter1;
         enter1 = new JButton("Select Story");
         enter1.setBackground(Color.white);
         enter1.setBounds(350, 400, 100, 50);
-
-        JTextField storyInput;
-        storyInput = new JTextField();
-        storyInput.setBounds(450, 400, 100, 50);
 
         JButton backBtn;
         backBtn = new JButton("GO BACK!");
@@ -39,54 +26,48 @@ public class MyFrame {
         l = new JLabel("MadLib+");
         l.setFont(new Font("Fonts", 3, 50));
 
-        ImageIcon icon = new ImageIcon("Panel Back.jpg");
-        icon.setImage(icon.getImage().getScaledInstance(600, 600, Image.SCALE_DEFAULT));
-        JLabel pic = new JLabel(icon);
-
         JPanel p = new JPanel();
         p.setBackground(Color.cyan);
-        p.add(pic);
         p.add(l);
+
+
+        String[] optionsToChoose = {"Horror", "Comedy", "Action"};
+        JComboBox<String> jComboBox = new JComboBox<>(optionsToChoose);
+        jComboBox.setBounds(445, 250, 140, 20);
 
         DefaultListModel<String> demoList = new DefaultListModel<>();
         demoList.addElement("Story 1");
         demoList.addElement("Story 2");
         demoList.addElement("Story 3");
-
         JList<String> listd = new JList<>(demoList);
         listd.setBounds(500, 450, 400, 200);
         listd.setBorder(BorderFactory.createLineBorder(Color.black));
 
         DefaultListModel<String> demoList1 = new DefaultListModel<>();
-        demoList1.addElement("Story 1");
-        demoList1.addElement("Story 2");
-        demoList1.addElement("Story 3");
-
-        JList<String> listd1 = new JList<>(demoList);
-        listd1.setBounds(500, 450, 400, 200);
-        listd1.setBorder(BorderFactory.createLineBorder(Color.black));
-
+        demoList1.addElement("Story 4");
+        demoList1.addElement("Story 5");
+        demoList1.addElement("Story 6");
+        JList<String> list1 = new JList<>(demoList1);
+        list1.setBounds(500, 450, 400, 200);
+        list1.setBorder(BorderFactory.createLineBorder(Color.black));
 
         DefaultListModel<String> demoList2 = new DefaultListModel<>();
-        demoList2.addElement("Story 1");
-        demoList2.addElement("Story 2");
-        demoList2.addElement("Story 3");
+        demoList2.addElement("Story 7");
+        demoList2.addElement("Story 8");
+        demoList2.addElement("Story 9");
+        JList<String> lists2 = new JList<>(demoList2);
+        lists2.setBounds(500, 450, 400, 200);
+        lists2.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        JList<String> listd2 = new JList<>(demoList);
-        listd2.setBounds(500, 450, 400, 200);
-        listd2.setBorder(BorderFactory.createLineBorder(Color.black));
 
-
-        f.add(enterB);
-        f.add(inputF);
         f.add(enter1);
-        f.add(storyInput);
+        f.add(jComboBox);
         f.add(listd);
-        f.add(listd1);
-        f.add(listd2);
+        f.add(list1);
+        f.add(lists2);
         listd.setVisible(false);
-        listd1.setVisible(false);
-        listd2.setVisible(false);
+        list1.setVisible(false);
+        lists2.setVisible(false);
         f.add(backBtn);
         backBtn.setVisible(true);
         f.add(p);
@@ -95,38 +76,23 @@ public class MyFrame {
         f.setLocationRelativeTo(null);
         f.setVisible(true);
 
-        enterB.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int color = 0;
-                p.setBackground(Color.white);
-                color++;
-                System.out.println(color);
-            }
-        });
 
         enter1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (Objects.equals(storyInput.getText(), "Horror")){
+                if (Objects.equals(jComboBox.getItemAt(0), "Horror")){
                     listd.setVisible(true);
-                    enterB.setVisible(false);
-                    inputF.setVisible(false);
-                    storyInput.setVisible(false);
+                    jComboBox.setVisible(false);
                     enter1.setVisible(false);
                 }
-                else if (Objects.equals(storyInput.getText(), "Comedy")){
-                    listd1.setVisible(true);
-                    enterB.setVisible(false);
-                    inputF.setVisible(false);
-                    storyInput.setVisible(false);
+                else if (Objects.equals(jComboBox.getItemAt(1), "Comedy")){
+                    list1.setVisible(true);
+                    jComboBox.setVisible(false);
                     enter1.setVisible(false);
                 }
-                else if (Objects.equals(storyInput.getText(), "Action")){
-                    listd2.setVisible(true);
-                    enterB.setVisible(false);
-                    inputF.setVisible(false);
-                    storyInput.setVisible(false);
+                else if (Objects.equals(jComboBox.getItemAt(2), "Action")){
+                    lists2.setVisible(true);
+                    jComboBox.setVisible(false);
                     enter1.setVisible(false);
                 }
             }
@@ -135,14 +101,11 @@ public class MyFrame {
         backBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                enterB.setVisible(true);
-                inputF.setVisible(true);
-                storyInput.setVisible(true);
-                storyInput.setText("");
+                jComboBox.setVisible(true);
                 enter1.setVisible(true);
                 listd.setVisible(false);
-                listd1.setVisible(false);
-                listd2.setVisible(false);
+                list1.setVisible(false);
+                lists2.setVisible(false);
             }
         });
     }
