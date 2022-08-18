2cd04f3a22d2d388668507c6dcadbd185d7ea4b6
joflores18 <joflores@student.neumont.edu>
2022-08-18 13:25:05 -0600

    Panel Changes

--- MadLIb/MyFrame.java
+++ MadLIb/MyFrame.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class MyFrame {
-    public void createFrame() throws IOException {
+    public void createFrame() {
 
         JFrame f;
         f = new JFrame("MadLib+");
