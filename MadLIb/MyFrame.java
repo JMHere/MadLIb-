0d417c355ca81e0c3d0c52ac22ec6b8184ec9564
JMHere <jmendez@student.neumont.edu>
2022-08-31 09:38:22 -0600

    I added two methods to the MyFrame

--- MadLIb/MyFrame.java
+++ MadLIb/MyFrame.java
@@ -4,8 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.util.Arrays;
 import java.util.Objects;
+
 
 public class MyFrame {
     public void createFrame() {
@@ -34,7 +35,7 @@ public class MyFrame {
         JTextPane random;
         random = new JTextPane();
         random.setBackground(Color.white);
-        random.setBounds(400, 490, 140, 35);
+        random.setBounds(350, 490, 300, 300);
 
         JLabel l;
         l = new JLabel("MadLib+");
@@ -44,6 +45,13 @@ public class MyFrame {
         p.setBackground(Color.cyan);
         p.add(l);
 
+        JTextField storyInput;
+        storyInput = new JTextField();
+        storyInput.setBounds(410, 250, 140, 20);
+
+        JButton storyIButton;
+        storyIButton = new JButton("Enter");
+        storyIButton.setBounds(410, 310, 140, 20);
 
         String[] optionsToChoose = {"Horror", "Comedy", "Action"};
         JComboBox<String> jComboBox = new JComboBox<>(optionsToChoose);
@@ -80,6 +88,10 @@ public class MyFrame {
         f.add(list1);
         f.add(list2);
         f.add(random);
+        f.add(storyInput);
+        f.add(storyIButton);
+        storyInput.setVisible(false);
+        storyIButton.setVisible(false);
         random.setVisible(false);
         list.setVisible(false);
         list1.setVisible(false);
@@ -147,6 +159,8 @@ public class MyFrame {
                 list1.setVisible(false);
                 list2.setVisible(false);
                 random.setVisible(false);
+                storyInput.setVisible(false);
+                storyIButton.setVisible(false);
             }
         });
 
@@ -155,7 +169,9 @@ public class MyFrame {
             public void actionPerformed(ActionEvent e) {
                 if (Objects.equals(storyChoice.getText(), "Story 1")){
                     random.setVisible(true);
-                    random.setText("This is the story you choose");
+                    random.setText(storyChange());
+                    storyInput.setVisible(true);
+                    storyIButton.setVisible(true);
                 }
                 else if (Objects.equals(storyChoice.getText(), "Story 2")){
                     random.setVisible(true);
@@ -175,5 +191,77 @@ public class MyFrame {
                 list2.setVisible(false);
             }
         });
+
+        storyIButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = storyInput.getText();
+                String[] parsed;
+                parsed = input.split(", ");
+
+                random.setText(storyChange(parsed));
+            }
+        });
+
+    }
+
+    public String storyChange() {
+        String story = "";
+
+        String adjective = "__Enter adjective__";
+        String adjective2 = "__Enter adjective__";
+        String bird = "__type of bird__";
+        String roomInHouse = "__Room in a house__";
+        String verbPast = "__verb(past tense)__";
+        String verb = "__Verb__";
+        String relative = "__relative's name__";
+        String noun = "__noun__";
+        String liquid = "__a liquid__";
+        String verbing = "__Verb ending in -ing__";
+        String partOfBodys = "__part of a boy (Plural)__";
+        String pNoun = "__plural noun__";
+        String verbing2 = "__verb ending in -ing__";
+        String noun2 = "__noun__";
+
+        story = "It was a " + adjective + ", cold November day I woke up to the " +adjective2
+                + " smell of " + bird + " roasting in the " + roomInHouse
+                + " downstairs. I " + verbPast + " down the stairs to see if I could help " + verb
+                + " the dinner. My mom said, 'See if " + relative + " needs a fresh " + noun
+                + ".' So I carried a tray of glasses full of " + liquid + " into the " +
+                verbing + " room. When I got there, I couldn't believe my " + partOfBodys +
+                "! There were " + pNoun + " " + verbing2 + " on the " + noun2 + "!";
+
+        return story;
+    }
+
+    public String storyChange(String[] array) {
+        String story = "";
+
+
+
+        String adjective = array[0];
+        String adjective2 = array[1];
+        String bird = array[2];
+        String roomInHouse = array[3];
+        String verbPast = array[4];
+        String verb = array[5];
+        String relative = array[6];
+        String noun = array[7];
+        String liquid = array[8];
+        String verbing = array[9];
+        String partOfBodys = array[10];
+        String pNoun = array[11];
+        String verbing2 = array[12];
+        String noun2 = array[13];
+
+        story = "It was a " + adjective + ", cold November day I woke up to the " +adjective2
+                + " smell of " + bird + " roasting in the " + roomInHouse
+                + " downstairs. I " + verbPast + " down the stairs to see if I could help " + verb
+                + " the dinner. My mom said, 'See if " + relative + " needs a fresh " + noun
+                + ".' So I carried a tray of glasses full of " + liquid + " into the " +
+                verbing + " room. When I got there, I couldn't believe my " + partOfBodys +
+                "! There were " + pNoun + " " + verbing2 + " on the " + noun2 + "!";
+
+        return story;
     }
 }
